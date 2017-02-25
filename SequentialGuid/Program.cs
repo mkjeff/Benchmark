84871a439e92d76ca6b70201c12d4af3091c0a24
Jeff.He <jeffhc.he@afusion.com>
2017-02-25 19:13:15 +0800

    Add Sequential Guid

---
+++ SequentialGuid/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace SequentialGuid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<SequentialGuidBenchmark>();
+
+            Console.WriteLine(summary);
+
+            Console.ReadLine();
+        }
+    }
+}
